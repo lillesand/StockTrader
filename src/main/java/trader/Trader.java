1766c99e21e78a4b527cf7c4422878a3b17835fe
Joran Vagnby Lillesand <lillesand@gmail.com>
2010-05-17 15:30:52 +0530

    added simple requirement for injection

--- src/main/java/trader/Trader.java
+++ src/main/java/trader/Trader.java
@@ -1,7 +1,15 @@
 package trader;
 
+import external.Market;
+
 public class Trader {
  
+    private final Market targetMarket;
+
+    public Trader(Market market) {
+        this.targetMarket = market;
+    }
+    
     public void makeTransaction() {
         //TODO: Implement
     }
